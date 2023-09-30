fa317bc7bfd10cd8a7ff9183483969cc917ee6d3
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-10-01 00:30:45 +0530

    ADD: registration pre_fnxlity

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,24 @@ import { Component } from '@angular/core';
 export class RegisterComponent {
   account_type: string = "";
   verify_otp: boolean = false;
+  submitBtnIsLoading:boolean = false;
   email:string = "xyz@gmail.com";
+
+  registration_form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required])
+  });
 
   onInputChange(otp:string){
     console.log(otp);
+  }
+
+  submitRgForm(){
+    this.submitBtnIsLoading = true;
+    setTimeout(() => {this.submitBtnIsLoading = false},3000);
+    console.log(this.registration_form.value);
+    
   }
 }
