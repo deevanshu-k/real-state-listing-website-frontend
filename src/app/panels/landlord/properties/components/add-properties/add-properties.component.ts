c4b2b058067ff896a71612dfed9be34ed13db93e
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-11-02 16:35:58 +0530

    ADD: landlordAddProperties

--- src/app/panels/landlord/properties/components/add-properties/add-properties.component.ts
+++ src/app/panels/landlord/properties/components/add-properties/add-properties.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { State, City } from 'country-state-city';
 
 @Component({
   selector: 'app-add-properties',
   templateUrl: './add-properties.component.html',
   styleUrls: ['./add-properties.component.css']
 })
-export class AddPropertiesComponent {
+export class AddPropertiesComponent implements OnInit {
+  property_type: { key: string, value: string }[] = [
+    { key: 'Room', value: 'ROOM' },
+    { key: 'Home', value: 'HOME' },
+    { key: 'Flat', value: 'FLAT' },
+    {
+      key: 'Shop', value: 'COMMERCIAL_SPACE'
+    }];
+  offer_type: { key: string, value: string }[] = [
+    { key: 'Rent', value: "RENT" },
+    { key: 'Sell', value: "SELL" }
+  ];
+  states: { code: string, name: string }[] = [];
+  districts: string[] = [];
+
+  ngOnInit(): void {
+    this.states = State.getStatesOfCountry('IN').map(d => {
+      return { code: d.isoCode, name: d.name };
+    });
+  }
+
+  stateChanged(code:string) {
+    this.districts = City.getCitiesOfState('IN',code).map(d => d.name);
+  }
 
 }
