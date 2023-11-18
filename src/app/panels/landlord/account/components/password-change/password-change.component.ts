c65bea8b9f45c7d1febf91c10edd1a457bab1df0
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-11-18 19:01:18 +0530

    ADD: passwordchange functionality for landlord

--- src/app/panels/landlord/account/components/password-change/password-change.component.ts
+++ src/app/panels/landlord/account/components/password-change/password-change.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { PasswordService } from 'src/app/services/password.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-password-change',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./password-change.component.css']
 })
 export class PasswordChangeComponent {
+  email: string = "";
+  // Btn Loading
+  changepswdbtnloading: boolean = false;
 
+  constructor(
+    private passwordService: PasswordService,
+    private snackbarService: SnackbarService,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    let decodedToken = this.authService.getDecodedToken(String(localStorage.getItem("token")));
+    this.email = decodedToken.email;
+  }
+
+  getPswdResetLink() {
+    this.changepswdbtnloading = true;
+    this.passwordService.requestResetLink().subscribe({
+      next: (d) => {
+        this.snackbarService.openSnackBar(d.message, "OK", "end", "bottom", 3000);
+        this.router.navigate(['/panel/landlord/account']);
+      },
+      error: (error) => {
+        this.snackbarService.openSnackBar(error.error.message, "OK", "end", "bottom", 3000);
+        this.router.navigate(['/panel/landlord/account']);
+      }
+    });
+  }
 }
