f85b00f7b6cdd7e4f1434d6b3ce75926f1faa196
Deevanshu Kushwah <deevanshukushwah80@gmail.com>
2023-11-24 01:16:37 +0530

    ADD: contact page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
     production: true,
-    END_POINT: "http://192.168.1.3:3000/api"
+    END_POINT: "http://3.84.126.3:3000/api",
+    PROPERTY_TYPES: [
+        { key: 'ROOM', label: 'Room' },
+        { key: 'HOUSE', label: 'House' },
+        { key: 'FLAT', label: 'Flat' },
+        { key: 'COMMERCIAL_SPACE', label: 'Commercial Space' }
+    ],
+    OFFER_TYPES: [
+        { label: 'Rent', key: "RENT" },
+        { label: 'Sell', key: "SELL" }
+    ]
 }
